459021d8d1da1246a04af254bc86326d86a67896
gexu <gexu@singhand.com>
2025-01-09 17:00:06 +0800

    feat: 初始化 Quarkus 项目结构和配置
    - 添加项目基础结构和配置文件
    - 设置数据库连接和 Liquibase 配置- 添加 Dockerfile 和 docker-compose 文件
    - 配置 Maven 和 Git 忽略文件
    - 添加简单的测试用例

---
+++ src/main/java/com/gexu/demo/rest/StudentResource.java
@@ -0,0 +1,59 @@
+package com.gexu.demo.rest;
+
+import com.gexu.demo.models.Student;
+import com.gexu.demo.models.result.PageResult;
+import io.quarkus.panache.common.Page;
+import io.quarkus.panache.common.Sort;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/student")
+public class StudentResource {
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  public PageResult<Student> getStudents(
+      @QueryParam("page") @DefaultValue("0") int pageIndex,
+      @QueryParam("size") @DefaultValue("10") int pageSize,
+      @QueryParam("sort") @DefaultValue("name") String sortField,
+      @QueryParam("order") @DefaultValue("asc") String sortOrder) {
+
+    final var page = Page.of(pageIndex, pageSize);
+    final var sort = Sort.by(sortField).direction(Sort.Direction.valueOf((sortOrder)));
+
+    final var query = Student.findAll(sort).page(page);
+
+    return new PageResult<>(query.list(), pageIndex, pageSize, query.pageCount(), query.count(),
+        query.hasNextPage(), query.hasPreviousPage());
+  }
+
+  @GET
+  @Path("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Student getStudent(@PathParam("id") String id) {
+
+    return Student.findById(id);
+  }
+
+  @POST
+  @Transactional
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Student addStudent(String name) {
+
+    final var student = new Student();
+    student.setName(name);
+    student.persist();
+    return student;
+  }
+}
